0a1547e5dfd924197e3130ade82e60d4d04f989e
vitormcal <142258537+vitormcal@users.noreply.github.com>
2023-08-15 13:01:10 -0300

    Update ex12.py

--- ex12.py
+++ ex12.py
@@ -1,3 +1,3 @@
 oldprice = float(input('enter the price of a product to find out the new value with a 5% discount: '))
 newprice = oldprice - oldprice*0.05
-print ('The new price with 5% off is: {}'.format(newprice))
+print ('The new price with 5% off is: {:.2f}'.format(newprice))

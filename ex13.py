975b637a700cd493c9ecd2fd394136930fb5dfa7
Vitor Martins <142258537+vitormcal@users.noreply.github.com>
2023-08-15 17:30:25 -0300

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,3 @@
+salarioa = float(input('Digite um salário para saber seu valor com 15% de aumento: R$'))
+salarion = salarioa + salarioa * 0.15
+print('O novo salário com 15% de aumento é de: R${:.2f}'.format(salarion))
